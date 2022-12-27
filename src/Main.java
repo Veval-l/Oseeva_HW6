8ca01b7972a3e105e4f265010ef16e83b5e0c3be
Oseeva <fselti1168rus@gmail.com>
2022-12-28 04:21:06 +0700

    Начальный коммит

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Homework!");
+        task1();
+
+
+    }
+    public static void task1() {
+        /**
+         *
+         */
+        System.out.println("Задача 1");
+
+
+    }
+}
